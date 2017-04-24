38e4e8300053c16193377b76f844536c2a64a5b3
weimingxuan <793270445@qq.com>
2017-04-24 11:16:41 +0800

    no message

---
+++ pages/posts/post.js
@@ -0,0 +1,21 @@
+var postsData = require("../../data/posts-data.js")
+
+Page({
+
+  data:{},
+
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+    this.setData({
+        posts_key:postsData.postList
+    });
+  },
+
+  onPostTap:function(event){
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: "post-detail/post-detail?id=" + postId
+    })
+  }
+
+})
